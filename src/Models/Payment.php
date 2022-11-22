65054f791d4aa9d08de134290a3d5710bbae95f0
Md Al imrun Khandakar <md.alimrun@gmail.com>
2022-11-22 12:06:26 +0600

    ok

--- src/Models/Payment.php
+++ src/Models/Payment.php
@@ -4,8 +4,10 @@ namespace Mdalimrun\CombinedPaymentLibrary\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Mdalimrun\CombinedPaymentLibrary\Traits\HasUuid;
 
 class Payment extends Model
 {
+    use HasUuid;
     use HasFactory;
 }
