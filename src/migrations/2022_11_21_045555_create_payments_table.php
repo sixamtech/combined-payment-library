e4825101f71dea29de71835f2288248505a3b68e
Md Al imrun Khandakar <md.alimrun@gmail.com>
2022-11-22 13:06:41 +0600

    k

--- src/migrations/2022_11_21_045555_create_payments_table.php
+++ src/migrations/2022_11_21_045555_create_payments_table.php
@@ -13,12 +13,14 @@ return new class extends Migration {
     public function up()
     {
         Schema::create('payments', function (Blueprint $table) {
-            $table->uuid();
+            $table->uuid('id');
             $table->string('unit_id',64)->nullable();
             $table->string('unit_name',64)->nullable();
             $table->string('customer_id',64)->nullable();
             $table->decimal('payment_amount', 24, 2)->default(0);
             $table->string('callback',191)->nullable();
+            $table->string('hook',191)->nullable();
+            $table->string('transaction_id',191)->nullable();
             $table->string('currency_code',10)->default('USD');
             $table->json('additional_data')->nullable();
             $table->string('payment_method',50)->nullable();
