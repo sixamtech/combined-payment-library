449fc94963b1767420a03815d5cb31aa766e4416
Md Al imrun Khandakar <md.alimrun@gmail.com>
2022-11-24 12:38:37 +0600

    paypal upgraded

--- src/Controllers/PaypalPaymentController.php
+++ src/Controllers/PaypalPaymentController.php
@@ -4,70 +4,147 @@ namespace Mdalimrun\CombinedPaymentLibrary\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\Validator;
+use Mdalimrun\CombinedPaymentLibrary\Models\Payment;
+use Mdalimrun\CombinedPaymentLibrary\Traits\Processor;
 use Srmklive\PayPal\Services\ExpressCheckout;
 
 class PaypalPaymentController extends Controller
 {
-    public function __construct()
-    {
+    use Processor;
 
+    private $config_values;
+
+    private Payment $payment;
+
+    public function __construct(Payment $payment)
+    {
+        $config = $this->payment_config('paypal', 'payment_config');
+        if (!is_null($config) && $config->mode == 'live') {
+            $this->config_values = json_decode($config->live_values);
+        } elseif (!is_null($config) && $config->mode == 'test') {
+            $this->config_values = json_decode($config->test_values);
+        }
+
+        $config = array(
+            'mode' => env('PAYPAL_MODE', $config->mode == 'live' ? 'live' : 'sandbox'),
+            'sandbox' => [
+                'username' => env('PAYPAL_SANDBOX_API_USERNAME', $this->config_values->username),
+                'password' => env('PAYPAL_SANDBOX_API_PASSWORD', $this->config_values->password),
+                'secret' => env('PAYPAL_SANDBOX_API_SECRET', $this->config_values->secret),
+                'certificate' => env('PAYPAL_SANDBOX_API_CERTIFICATE', $this->config_values->certificate),
+                'app_id' => $this->config_values->app_id,
+            ],
+
+            'live' => [
+                'username' => env('PAYPAL_LIVE_API_USERNAME', $this->config_values->username),
+                'password' => env('PAYPAL_LIVE_API_PASSWORD', $this->config_values->password),
+                'secret' => env('PAYPAL_LIVE_API_SECRET', $this->config_values->secret),
+                'certificate' => env('PAYPAL_LIVE_API_CERTIFICATE', $this->config_values->certificate),
+                'app_id' => $this->config_values->app_id,
+            ],
+            'payment_action' => 'Sale',
+            'currency' => env('PAYPAL_CURRENCY', 'USD'),
+            'billing_type' => 'MerchantInitiatedBilling',
+            'notify_url' => '',
+            'locale' => '',
+            'validate_ssl' => false,
+        );
+        Config::set('paypal', $config);
+
+        $this->payment = $payment;
     }
 
     /**
      * Responds with a welcome message with instructions
      *
-     * @return \Illuminate\Http\Response
      */
-    public function payment()
+    public function payment(Request $request)
     {
-        $data = [];
-        $data['items'] = [
+        $validator = Validator::make($request->all(), [
+            'payment_id' => 'required|uuid'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($this->response_formatter(DEFAULT_400, null, $this->error_processor($validator)), 400);
+        }
+
+        $data = $this->payment::where(['id' => $request['payment_id']])->where(['is_paid' => 0])->first();
+        if (!isset($data)) {
+            return response()->json($this->response_formatter(DEFAULT_204), 200);
+        }
+
+        if ($data['additional_data'] != null) {
+            $business = json_decode($data['additional_data']);
+            $business_name = $business->business_name ?? "my_business";
+        } else {
+            $business_name = "my_business";
+        }
+
+        $payment_data = [];
+        $payment_data['items'] = [
             [
-                'name' => 'ItSolutionStuff.com',
-                'price' => 100,
-                'desc'  => 'Description for ItSolutionStuff.com',
+                'name' => $business_name,
+                'price' => round($data->payment_amount, 2),
+                'desc' => 'payment ID :' . $data->id,
                 'qty' => 1
             ]
         ];
 
-        $data['invoice_id'] = 1;
-        $data['invoice_description'] = "Order #{$data['invoice_id']} Invoice";
-        $data['return_url'] = route('payment.success');
-        $data['cancel_url'] = route('payment.cancel');
-        $data['total'] = 100;
+        $payment_data['invoice_id'] = $data->id;
+        $payment_data['invoice_description'] = "Order #{$payment_data['invoice_id']} Invoice";
+        $payment_data['return_url'] = route('paypal.success', ['payment_id' => $data->id]);
+        $payment_data['cancel_url'] = route('paypal.cancel', ['payment_id' => $data->id]);
+        $payment_data['total'] = round($data->payment_amount, 2);
 
         $provider = new ExpressCheckout;
-
-        $response = $provider->setExpressCheckout($data);
-
-        $response = $provider->setExpressCheckout($data, true);
+        $response = $provider->setExpressCheckout($payment_data);
+        //$response = $provider->setExpressCheckout($data, true);
 
         return redirect($response['paypal_link']);
     }
 
     /**
      * Responds with a welcome message with instructions
-     *
-     * @return \Illuminate\Http\Response
      */
-    public function cancel()
+    public function cancel(Request $request)
     {
-        dd('Your payment is canceled. You can create cancel page here.');
+        $data = $this->payment::where(['id' => $request['payment_id']])->first();
+        if ($data['callback'] != null) {
+            return redirect($data['callback'] . '?payment_status=cancel');
+        }
+        return response()->json($this->response_formatter(DEFAULT_204), 200);
     }
 
     /**
      * Responds with a welcome message with instructions
-     *
-     * @return \Illuminate\Http\Response
      */
     public function success(Request $request)
     {
+        $provider = new ExpressCheckout;
         $response = $provider->getExpressCheckoutDetails($request->token);
 
         if (in_array(strtoupper($response['ACK']), ['SUCCESS', 'SUCCESSWITHWARNING'])) {
-            dd('Your payment was successfully. You can create success page here.');
+            $data = $this->payment::where(['id' => $request['payment_id']])->first();
+            if (isset($data) && function_exists($data->hook)) {
+                call_user_func($data->hook, [
+                    'payment_method' => 'paypal',
+                    'transaction_id' => $request->input('payment_id'),
+                    'payment_id' => $request->input('payment_id'),
+                ]);
+                $this->payment::where(['id' => $request['payment_id']])->update([
+                    'payment_method' => 'paypal',
+                    'is_paid' => 1,
+                    'transaction_id' => $request->input('payment_id'),
+                ]);
+            }
+            if ($data['callback'] != null) {
+                return redirect($data['callback'] . '?payment_status=success');
+            }
+            return response()->json($this->response_formatter(DEFAULT_200), 200);
         }
 
-        dd('Something is wrong.');
+        return response()->json($this->response_formatter(DEFAULT_404), 200);
     }
 }
--- src/routes.php
+++ src/routes.php
@@ -6,6 +6,7 @@ use Mdalimrun\CombinedPaymentLibrary\Controllers\FlutterwaveV3Controller;
 use Mdalimrun\CombinedPaymentLibrary\Controllers\LiqPayController;
 use Mdalimrun\CombinedPaymentLibrary\Controllers\MercadoPagoController;
 use Mdalimrun\CombinedPaymentLibrary\Controllers\PaymobController;
+use Mdalimrun\CombinedPaymentLibrary\Controllers\PaypalPaymentController;
 use Mdalimrun\CombinedPaymentLibrary\Controllers\PaystackController;
 use Mdalimrun\CombinedPaymentLibrary\Controllers\PaytmController;
 use Mdalimrun\CombinedPaymentLibrary\Controllers\RazorPayController;
@@ -38,9 +39,9 @@ Route::group(['prefix' => 'payment'], function () {
 
     //PAYPAL
     Route::group(['prefix' => 'paypal', 'as' => 'paypal.'], function () {
-        Route::get('pay', 'PaypalPaymentController@index');
-        Route::any('callback', 'PaypalPaymentController@callback')->name('callback');
-        Route::any('failed', 'PaypalPaymentController@failed')->name('failed');
+        Route::get('pay', [PaypalPaymentController::class, 'payment']);
+        Route::any('callback', [PaypalPaymentController::class, 'callback'])->name('callback');
+        Route::any('canceled',  [PaypalPaymentController::class,'canceled'])->name('canceled');
     });
 
     //SENANG-PAY
@@ -57,8 +58,8 @@ Route::group(['prefix' => 'payment'], function () {
 
     //FLUTTERWAVE
     Route::group(['prefix' => 'flutterwave-v3', 'as' => 'flutterwave-v3.'], function () {
-        Route::get('pay', [FlutterwaveV3Controller::class,'initialize'])->name('pay');
-        Route::get('callback', [FlutterwaveV3Controller::class,'callback'])->name('callback');
+        Route::get('pay', [FlutterwaveV3Controller::class, 'initialize'])->name('pay');
+        Route::get('callback', [FlutterwaveV3Controller::class, 'callback'])->name('callback');
     });
 
     //PAYSTACK
@@ -71,30 +72,30 @@ Route::group(['prefix' => 'payment'], function () {
     //BKASH
     Route::group(['prefix' => 'bkash'], function () {
         // Payment Routes for bKash
-        Route::post('get-token', [BkashPaymentController::class,'getToken'])->name('bkash-get-token');
-        Route::post('create-payment', [BkashPaymentController::class,'createPayment'])->name('bkash-create-payment');
-        Route::post('execute-payment', [BkashPaymentController::class,'executePayment'])->name('bkash-execute-payment');
-        Route::get('query-payment',  [BkashPaymentController::class,'queryPayment'])->name('bkash-query-payment');
-        Route::post('success',  [BkashPaymentController::class,'bkashSuccess'])->name('bkash-success');
-        Route::get('callback', [BkashPaymentController::class,'callback'])->name('bkash-callback');
+        Route::post('get-token', [BkashPaymentController::class, 'getToken'])->name('bkash-get-token');
+        Route::post('create-payment', [BkashPaymentController::class, 'createPayment'])->name('bkash-create-payment');
+        Route::post('execute-payment', [BkashPaymentController::class, 'executePayment'])->name('bkash-execute-payment');
+        Route::get('query-payment', [BkashPaymentController::class, 'queryPayment'])->name('bkash-query-payment');
+        Route::post('success', [BkashPaymentController::class, 'bkashSuccess'])->name('bkash-success');
+        Route::get('callback', [BkashPaymentController::class, 'callback'])->name('bkash-callback');
     });
 
     //PAYSTACK
     Route::group(['prefix' => 'liqpay', 'as' => 'liqpay.'], function () {
-        Route::get('payment', [LiqPayController::class,'payment'])->name('payment');
-        Route::any('callback', [LiqPayController::class,'callback'])->name('callback');
+        Route::get('payment', [LiqPayController::class, 'payment'])->name('payment');
+        Route::any('callback', [LiqPayController::class, 'callback'])->name('callback');
     });
 
     //MERCADOPAGO
     Route::group(['prefix' => 'mercadopago', 'as' => 'mercadopago.'], function () {
-        Route::get('pay', [MercadoPagoController::class,'index'])->name('index');
-        Route::post('make-payment', [MercadoPagoController::class,'make_payment'])->name('make_payment');
+        Route::get('pay', [MercadoPagoController::class, 'index'])->name('index');
+        Route::post('make-payment', [MercadoPagoController::class, 'make_payment'])->name('make_payment');
     });
 
     // paymob
     Route::group(['prefix' => 'paymob', 'as' => 'paymob.'], function () {
-        Route::any('pay', [PaymobController::class,'credit'])->name('pay');
-        Route::any('callback', [PaymobController::class,'callback'])->name('callback');
+        Route::any('pay', [PaymobController::class, 'credit'])->name('pay');
+        Route::any('callback', [PaymobController::class, 'callback'])->name('callback');
     });
 
 });
